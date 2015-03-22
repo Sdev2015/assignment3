71c575d0820b568647cfe62079db09905b0919d3
Shantanu Dev <devshantanu@gmail.com>
2015-03-22 17:49:43 -0400

    First commit

---
+++ cachematrix.R
@@ -0,0 +1,36 @@
+## Put comments here that give an overall description of what your
+## functions do
+##Invrese a matrix
+
+## Write a short comment describing this function
+
+> makeCacheMatrix <- function(x = matrix()) {
++     m<-NULL
++     set<-function(y){
++         x<<-y
++         m<<-NULL
++     }
++     get<-function() x
++     setmatrix<-function(solve) m<<- solve
++     getmatrix<-function() m
++     list(set=set, get=get,
++          setmatrix=setmatrix,
++          getmatrix=getmatrix)
++ }
+> 
+
+
+## Write a short comment describing this function
+##Determining the caching or no cached matrix
+
+> cacheSolve <- function(x=matrix(), ...) {
++     m<-x$getmatrix()
++     if(!is.null(m)){
++         message("You are getting cached data")
++         return(m)
++     }
++     matrix<-x$get()
++     m<-solve(matrix, ...)
++     x$setmatrix(m)
++     m
++ }
