225dd21e370986a0013e0a2784a3aca213fc1523
Shantanu Dev <devshantanu@gmail.com>
2015-03-22 18:38:13 -0400

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-##Invrese a matrix
+##Invrese a matrix. Thhis program will calculate the inverse.
 
 ## Write a short comment describing this function
 
